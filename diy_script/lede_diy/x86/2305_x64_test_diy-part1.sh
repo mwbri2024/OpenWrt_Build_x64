3af8c501b6211be54ce46583588398794ec9c13f
gxnas <52808122+gxnas@users.noreply.github.com>
2025-11-01 11:48:47 +0800

    Update 2305_x64_test_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
@@ -8,7 +8,8 @@
 
 # Add a feed source
 sed -i "/helloworld/d" feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
